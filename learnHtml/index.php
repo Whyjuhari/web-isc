8b27dacfe22384762147e6d5e3973fa9d816e6d4
Juhari <91404024+Whyjuhari@users.noreply.github.com>
2024-02-08 20:56:48 +0700

    Komitan Pertama

---
+++ learnHtml/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mari Belajar</title>
+
+    <style>
+    tr>:nth-child(1) {
+        font-weight: 600;
+    }
+    </style>
+</head>
+
+<body>
+
+    <div class="pembungkus">
+        <h3>BELAJAR HTML 2024</h3>
+        <table border="1" cellpadding="10" cellspacing="0">
+            <tr>
+                <td width="100">Nama</td>
+                <td width="100">Juhari</td>
+            </tr>
+            <tr>
+                <td>Nim</td>
+                <td>D0221322</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>Informatika</td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>Sumarrang, Campalagian</td>
+            </tr>
+            <tr>
+                <td>Keahlian</td>
+                <td>
+                    <select>
+                        <option value="-----">------</option>
+                        <option value="c0py p@5te">c0py p@5te</option>
+                        <option value="penikmat template">penikmat template</option>
+                    </select>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+</body>
+
+</html>
